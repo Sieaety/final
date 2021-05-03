ae1a4b4e879bfb395ee7e65ab901c7ec3d4fa56b
Sieaety <kzorncom@badbrains.reclaimhosting.com>
2021-05-03 19:17:30 +0000

    added column arrays

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
-//TO DO NEXT: begin coding functions for actual game play, start by making arrays spaces for individual columns and array of all of the columns together
-currentPlayer = 2;
+//TO DO NEXT: function that detects space the user clicked
+currentPlayer = 1;
 spaces = document.querySelectorAll(".space");
 
-/*looping through all spaces on the board*/
+//colunms on the board from left to right
+col0 = [0, 7, 14, 21, 28, 35];
+col1 = [1, 8, 15, 22, 29, 36];
+col2 = [2, 9, 16, 23, 30, 37];
+col3 = [3, 10, 17, 24, 31, 38];
+col4 = [4, 11, 18, 25, 32, 39];
+col5 = [5, 12, 19, 26, 33, 40];
+col6 = [6, 13, 20, 27, 34, 41];
+
+//array of all columns
+allCol = [col0, col1, col2, col3, col4, col5, col6];
+
+//looping through all spaces on the board
 
 for (let i = 0; i < spaces.length; i++) {
   
-  /*changes color of space to the current player color when they hover over it*/
+  //changes color of space to the current player color when they hover over it
 
   if (currentPlayer == 1) {
     spaces[i].onmouseover = function() {
